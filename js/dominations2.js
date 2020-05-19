9ffa4e711effe472d68b7268f2ee054e39f6f560
canontank <58597814+canontank@users.noreply.github.com>
2020-05-19 15:40:57 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -24,6 +24,11 @@ function setTask12() {
 	taskList.push(new Array(type, "방공타워", "Lv. 3", " 700만 골드",  8,  0));
 	taskList.push(new Array(type, "방공타워", "Lv. 4", " 860만 골드", 10,  0));
 	taskList.push(new Array(type, "방공타워", "Lv. 5", "1060만 골드", 10, 12));
+	taskList.push(new Array(type, "방공타워", "Lv. 6", "1220만 골드", 11,  0));
+	taskList.push(new Array(type, "방공타워", "Lv. 7", "1400만 골드", 12,  0));
+	taskList.push(new Array(type, "방공타워", "Lv. 8", "1500만 골드", 13,  0));
+	taskList.push(new Array(type, "방공타워", "Lv. 9", "1700만 골드", 14, 12));
+	taskList.push(new Array(type, "방공타워", "Lv.10", "1800만 골드", 16,  5)); // 미확인
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
@@ -35,6 +40,7 @@ function setTask13() {
 	taskMap.title = "1-3. 국민 (3명 → 4명)";
 	taskMap.startDate = "2020-05-19 14:58:00";
 	taskList.push(new Array(type, "유정    ", "Lv.11", "1600만 골드", 14, 12)); // 디지털
+	taskList.push(new Array(type, "방공타워", "Lv. 4", " 860만 골드", 10,  0));
 	taskList.push(new Array(type, "방공타워", "Lv. 4", " 860만 골드", 10,  0));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
