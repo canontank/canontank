ea08c389c54a5124fa357db2e35c83ef0c900b07
canontank <58597814+canontank@users.noreply.github.com>
2020-05-27 12:04:06 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -87,14 +87,13 @@ function setTask21() {
 	var taskMap = new Object();
 	var type = "무기1";
 	taskMap.title = "2-1. 무기고 연구";
-	taskMap.startDate = "2020-05-15 22:28:00";
-	taskList.push(new Array(type, "개틀링건", "Lv. 7", "14.5만 석유", 11, 10 ));
-    taskList.push(new Array(type, "전술헬기", "Lv. 6", "  25만 석유", 16,  4 )); // 디지털
-    taskList.push(new Array(type, "무장차량", "Lv. 6", "  24만 석유", 14,  8 )); // 예상
-    taskList.push(new Array(type, "무장차량", "Lv. 7", "  25만 석유", 16,  4 )); // 예상
-    taskList.push(new Array(type, "무장차량", "Lv. 8", "  25만 석유", 16,  4 )); // 디지털
-    taskList.push(new Array(type, "엠알엘  ", "Lv. 4", "21.5만 석유", 14,  6 ));
-    taskList.push(new Array(type, "엠알엘  ", "Lv. 5", "  24만 석유", 14,  6 ));
+	taskMap.startDate = "2020-05-27 09:03:00";
+    	taskList.push(new Array(type, "전술헬기", "Lv. 6", "  25만 석유", 16,  4 )); // 디지털
+    	taskList.push(new Array(type, "무장차량", "Lv. 6", "  24만 석유", 14,  8 )); // 예상
+    	taskList.push(new Array(type, "무장차량", "Lv. 7", "  25만 석유", 16,  4 )); // 예상
+    	taskList.push(new Array(type, "무장차량", "Lv. 8", "  25만 석유", 16,  4 )); // 디지털
+    	taskList.push(new Array(type, "엠알엘  ", "Lv. 4", "21.5만 석유", 14,  6 ));
+    	taskList.push(new Array(type, "엠알엘  ", "Lv. 5", "  24만 석유", 14,  6 ));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
@@ -106,11 +105,11 @@ function setTask22() {
 	taskMap.title = "2-2. 무기고 연구";
 	taskMap.startDate = "2020-05-23 17:25:00";
 	taskList.push(new Array(type, "중전차  ", "Lv.10", "  23만 석유", 16,  4 ));
-    taskList.push(new Array(type, "개틀링건", "Lv. 8", "  20만 석유", 12,  9 )); // 예상
-    taskList.push(new Array(type, "개틀링건", "Lv. 9", "  23만 석유", 13, 10 )); // 예상
-    taskList.push(new Array(type, "개틀링건", "Lv.10", "  24만 석유", 14,  8 )); // 예상
-    taskList.push(new Array(type, "개틀링건", "Lv.11", "  25만 석유", 16,  4 )); // 예상
-    taskList.push(new Array(type, "개틀링건", "Lv.12", "  25만 석유", 16,  4 )); // 디지털
+    	taskList.push(new Array(type, "개틀링건", "Lv. 8", "  20만 석유", 12,  9 )); // 예상
+    	taskList.push(new Array(type, "개틀링건", "Lv. 9", "  23만 석유", 13, 10 )); // 예상
+    	taskList.push(new Array(type, "개틀링건", "Lv.10", "  24만 석유", 14,  8 )); // 예상
+    	taskList.push(new Array(type, "개틀링건", "Lv.11", "  25만 석유", 16,  4 )); // 예상
+    	taskList.push(new Array(type, "개틀링건", "Lv.12", "  25만 석유", 16,  4 )); // 디지털
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
