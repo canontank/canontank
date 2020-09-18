1609a6c943c92be4763e39c8e97ca56e7a8a5290
canontank <58597814+canontank@users.noreply.github.com>
2020-09-19 02:02:59 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -3,7 +3,7 @@ function setTask11() {
 	var taskMap = new Object();
 	var type = "국민1";
 	taskMap.title = "1-1. 국민 (4명)";
-	taskMap.startDate = "2020-09-15 01:29:00";
+	taskMap.startDate = "2020-09-19 01:29:00";
 	taskList.push(new Array(type, "방공타워", "Lv. 8", "1500만 골드", 13,  0));
 	taskList.push(new Array(type, "방공타워", "Lv. 9", "1700만 골드", 14, 12));
 	taskList.push(new Array(type, "방공타워", "Lv. 9", "1700만 골드", 14, 12));
@@ -17,7 +17,7 @@ function setTask12() {
 	var taskMap = new Object();
 	var type = "국민2";
 	taskMap.title = "1-2. 국민 (4명)";
-	taskMap.startDate = "2020-09-15 01:47:00";
+	taskMap.startDate = "2020-09-19 01:47:00";
 	taskList.push(new Array(type, "방공타워", "Lv. 9", "1700만 골드", 14, 12));
 	taskList.push(new Array(type, "방공타워", "Lv. 9", "1700만 골드", 14, 12));
 	taskList.push(new Array(type, "방공타워", "Lv.10", "1800만 골드", 16,  6));
@@ -31,7 +31,7 @@ function setTask13() {
 	var taskMap = new Object();
 	var type = "국민3";
 	taskMap.title = "1-3. 국민 (4명)";
-	taskMap.startDate = "2020-09-15 01:29:00";
+	taskMap.startDate = "2020-09-19 01:29:00";
 	taskList.push(new Array(type, "방공타워", "Lv. 8", "1500만 골드", 13,  0));
 	taskList.push(new Array(type, "방공타워", "Lv. 9", "1700만 골드", 14, 12));
 	taskList.push(new Array(type, "방공타워", "Lv.10", "1800만 골드", 16,  6));
@@ -45,7 +45,7 @@ function setTask14() {
 	var taskMap = new Object();
 	var type = "국민4";
 	taskMap.title = "1-4. 국민 (3명)";
-	taskMap.startDate = "2020-09-15 01:26:00";
+	taskMap.startDate = "2020-09-19 01:26:00";
 	taskList.push(new Array(type, "미샬격납", "Lv. 9", "  25만 석유", 17,  2 ));
 	taskList.push(new Array(type, "유정    ", "Lv.12", "1800만 골드", 15, 22 ));
 	taskList.push(new Array(type, "유정    ", "Lv.12", "1800만 골드", 15, 22 ));
@@ -59,7 +59,7 @@ function setTask15() {
 	var taskMap = new Object();
 	var type = "국민5";
 	taskMap.title = "1-5. 국민 (3명)";
-	taskMap.startDate = "2020-09-15 01:26:00";
+	taskMap.startDate = "2020-09-19 01:26:00";
 	taskList.push(new Array(type, "미샬격납", "Lv. 9", "  25만 석유", 17,  2 ));
 	taskList.push(new Array(type, "유정    ", "Lv.12", "1800만 골드", 15, 22 ));
 	taskList.push(new Array(type, "유정    ", "Lv.12", "1800만 골드", 15, 22 ));
