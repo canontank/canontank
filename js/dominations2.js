8ba4426683be591c884da1a575a3f5610b47dc35
canontank <58597814+canontank@users.noreply.github.com>
2021-02-09 23:06:10 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -86,8 +86,7 @@ function setTask22() {
 	var taskMap = new Object();
 	var type = "무기2";
 	taskMap.title = "2-2. 무기고 연구";
-	taskMap.startDate = "2021-02-01 20:30:00";
-	taskList.push(new Array(type, "정보수송", "Lv. 6", "  19만 석유",  9,  0 ));
+	taskMap.startDate = "2021-02-09 23:05:00";
 	taskList.push(new Array(type, "정보정찰", "Lv. 1", "  19만 석유",  9,  0 ));
 	taskList.push(new Array(type, "정보정찰", "Lv. 2", "  19만 석유",  9,  0 ));
 	taskList.push(new Array(type, "정보정찰", "Lv. 3", "  19만 석유",  9,  0 ));
