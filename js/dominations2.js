34ce9f527d22736bc103d19e79c6889e13e3af53
canontank <58597814+canontank@users.noreply.github.com>
2021-03-22 18:28:23 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -34,8 +34,7 @@ function setTask13() {
 	var taskMap = new Object();
 	var type = "국민3";
 	taskMap.title = "1-3. 국민 (4명)";
-	taskMap.startDate = "2021-03-15 17:40:00";
-	taskList.push(new Array(type, "골드창고", "Lv.13", " 320만 식량",  7,  0));
+	taskMap.startDate = "2021-03-22 18:27:00";
 	taskList.push(new Array(type, "골드창고", "Lv.14", " 470만 식량",  8,  0));
 	taskList.push(new Array(type, "골드창고", "Lv.15", " 600만 식량",  9,  0));
 	taskList.push(new Array(type, "골드창고", "Lv.16", " 850만 식량", 10, 12));
@@ -49,8 +48,7 @@ function setTask14() {
 	var taskMap = new Object();
 	var type = "국민4";
 	taskMap.title = "1-4. 국민 (4명)";
-	taskMap.startDate = "2021-03-15 17:40:00";
-	taskList.push(new Array(type, "식량창고", "Lv.13", " 320만 골드",  7,  0));
+	taskMap.startDate = "2021-03-22 18:27:00";
 	taskList.push(new Array(type, "식량창고", "Lv.14", " 470만 골드",  8,  0));
 	taskList.push(new Array(type, "식량창고", "Lv.15", " 600만 골드",  9,  0));
 	taskList.push(new Array(type, "식량창고", "Lv.16", " 850만 골드", 10, 12));
