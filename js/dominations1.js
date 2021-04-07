08489143524386fad96f8884ef0fccfe9612a169
canontank <58597814+canontank@users.noreply.github.com>
2021-04-07 16:16:04 +0900

    Update dominations1.js

--- js/dominations1.js
+++ js/dominations1.js
@@ -23,6 +23,8 @@ function setTask12() {
 	taskList.push(new Array(type, "식량창고", "Lv.15", " 600만 골드",  9,  0));
 	taskList.push(new Array(type, "식량창고", "Lv.16", " 850만 골드", 10, 12));
 	taskList.push(new Array(type, "식량창고", "Lv.17", "1050만 골드", 11,  0));
+	taskList.push(new Array(type, "방공타워", "Lv.11", "1900만 골드", 16, 12));
+	taskList.push(new Array(type, "방공타워", "Lv.11", "1900만 골드", 16, 12));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
@@ -34,7 +36,16 @@ function setTask13() {
 	taskMap.title = "1-3. 국민 (4명)";
 	taskMap.startDate = "2021-04-06 16:23:00";
 	taskList.push(new Array(type, "의회    ", "Lv. 5", "1200만 골드", 14,  0));
-	taskList.push(new Array(type, "방공타워", "Lv. ?", "1400만 골드", 14,  0)); // 미확인
+	taskList.push(new Array(type, "방공타워", "Lv. 2", " 550만 골드",  7,  0));
+	taskList.push(new Array(type, "방공타워", "Lv. 3", " 700만 골드",  8,  0));
+	taskList.push(new Array(type, "방공타워", "Lv. 4", " 860만 골드", 10,  0));
+	taskList.push(new Array(type, "방공타워", "Lv. 5", "1060만 골드", 10, 12));
+	taskList.push(new Array(type, "방공타워", "Lv. 6", "1220만 골드", 11,  0));
+	taskList.push(new Array(type, "방공타워", "Lv. 7", "1400만 골드", 12,  0));
+	taskList.push(new Array(type, "방공타워", "Lv. 8", "1500만 골드", 13,  0));
+	taskList.push(new Array(type, "방공타워", "Lv. 9", "1700만 골드", 14, 12));
+	taskList.push(new Array(type, "방공타워", "Lv.10", "1800만 골드", 16,  5));
+	taskList.push(new Array(type, "방공타워", "Lv.11", "1900만 골드", 16, 12));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
@@ -49,6 +60,8 @@ function setTask14() {
 	taskList.push(new Array(type, "골드창고", "Lv.15", " 600만 식량",  9,  0));
 	taskList.push(new Array(type, "골드창고", "Lv.16", " 850만 식량", 10, 12));
 	taskList.push(new Array(type, "골드창고", "Lv.17", "1050만 식량", 11,  0));
+	taskList.push(new Array(type, "방공타워", "Lv.11", "1900만 골드", 16, 12));
+	taskList.push(new Array(type, "방공타워", "Lv.11", "1900만 골드", 16, 12));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
