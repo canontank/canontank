494fec61338374898c7e5fbe96a8a5e938771e3d
canontank <58597814+canontank@users.noreply.github.com>
2021-04-24 05:35:56 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -61,7 +61,7 @@ function setTask21() {
 	taskMap.startDate = "2021-04-11 21:50:00";
 	taskList.push(new Array(type, "중전차  ", "Lv.12", "  24만 석유", 17,  2 ));
 	taskList.push(new Array(type, "게릴라  ", "Lv.13", "  24만 석유", 17,  2 ));
-	taskList.push(new Array(type, "개틀링  ", "Lv.14", "  24만 석유", 17,  2 ));
+	taskList.push(new Array(type, "에이피씨", "Lv. 9", "  26만 석유", 17,  2 ));
 	taskList.push(new Array(type, "엠알엘  ", "Lv. 7", "  26만 석유", 15,  4 ));
 	taskList.push(new Array(type, "응급처치", "Lv. 8", " 820만 식량", 10, 22 ));
 	taskList.push(new Array(type, "파괴    ", "Lv. 6", " 970만 식량", 10, 22 ));
@@ -75,9 +75,8 @@ function setTask22() {
 	var type = "무기2";
 	taskMap.title = "2-2. 무기고 연구";
 	taskMap.startDate = "2021-04-05 17:27:00";
-	taskList.push(new Array(type, "바주카  ", "Lv.12", "  24만 석유", 17,  2 ));
 	taskList.push(new Array(type, "무장차량", "Lv.11", "  26만 석유", 17,  2 ));
-	taskList.push(new Array(type, "에이피씨", "Lv. 9", "  26만 석유", 17,  2 ));
+	taskList.push(new Array(type, "개틀링  ", "Lv.14", "  24만 석유", 17,  2 ));
 	taskList.push(new Array(type, "전술헬기", "Lv. 6", "  26만 석유", 17,  2 ));
 	taskList.push(new Array(type, "일제사격", "Lv. 8", " 730만 식량", 10, 22 ));
 	taskList.push(new Array(type, "배반    ", "Lv. 5", " 970만 식량", 10, 22 ));
