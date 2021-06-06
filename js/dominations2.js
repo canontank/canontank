faa3311580f60293c0c6805a0a5763742a1420f5
canontank <58597814+canontank@users.noreply.github.com>
2021-06-07 03:30:49 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -3,8 +3,8 @@ function setTask11() {
 	var taskMap = new Object();
 	var type = "국민1";
 	taskMap.title = "1-1. 국민 (3명)";
-	taskMap.startDate = "2021-05-12 23:16:00";
-	taskList.push(new Array(type, "유정    ", "Lv.13", "2000만 골드", 16, 12 ));
+	taskMap.startDate = "2021-06-07 03:30:00";
+	taskList.push(new Array(type, "씨지아이", "Lv. 1", "  10만 석유", 15, 12 ));
 	taskList.push(new Array(type, "유정    ", "Lv.13", "2000만 골드", 16, 12 ));
 	taskList.push(new Array(type, "유정    ", "Lv.13", "2000만 골드", 16, 12 ));
 	taskList.push(new Array(type, "유정    ", "Lv.13", "2000만 골드", 16, 12 ));
