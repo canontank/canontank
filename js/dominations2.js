c20f9eb8f10a154172ee20fedea7673d743cfe53
canontank <58597814+canontank@users.noreply.github.com>
2021-09-18 10:08:37 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -77,8 +77,8 @@ function setTask21() {
 	var taskMap = new Object();
 	var type = "무기1";
 	taskMap.title = "2-1. 무기고 연구";
-	taskMap.startDate = "2021-09-01 22:35:00";
-	taskList.push(new Array(type, "포병대  ", "Lv. 1", "1520만 식량", 13,  7 ));
+	taskMap.startDate = "2021-09-18 09:52:00";
+	taskList.push(new Array(type, "궁수  ", "Lv. 2", "1800만 식량", 15,  5 ));
 	taskList.push(new Array(type, "헬리곱터", "Lv. 1", "1520만 식량", 13,  7 ));
 	taskList.push(new Array(type, "강습차량", "Lv. 1", "1780만 식량", 14,  6 ));
 	taskList.push(new Array(type, "포병대  ", "Lv. 2", "1800만 식량", 15,  5 ));
@@ -98,7 +98,6 @@ function setTask22() {
 	taskMap.startDate = "2021-09-17 14:00:00";
 	taskList.push(new Array(type, "보병    ", "Lv. 2", "1800만 식량", 15,  5 ));
 	taskList.push(new Array(type, "호송차  ", "Lv. 1", "1505만 식량", 13,  7 ));
-	taskList.push(new Array(type, "궁수    ", "Lv. 2", "1800만 식량", 15,  5 ));
 	taskList.push(new Array(type, "돌격대  ", "Lv. 2", "1800만 식량", 15,  5 ));
 	taskList.push(new Array(type, "전차    ", "Lv. 2", "1800만 식량", 15,  5 ));
 	taskList.push(new Array(type, "공병    ", "Lv. 2", "1800만 식량", 15,  5 ));
