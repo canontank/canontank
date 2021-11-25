20c97761e0081c308cfa8207a37cf84ddfba1ae4
canontank <58597814+canontank@users.noreply.github.com>
2021-11-25 21:16:54 +0900

    Update dominations1.js

--- js/dominations1.js
+++ js/dominations1.js
@@ -73,6 +73,17 @@ function setTask21() {
 	taskMap.title = "2-1. 무기고 연구";
 	taskMap.startDate = "2021-11-25 21:13:00";
 	taskList.push(new Array(type, "클레오  ", "Lv.21", "   4만 석유",  2, 20));
+	taskList.push(new Array(type, "맥아더  ", "Lv.11", "   4만 석유",  1, 22));
+	taskList.push(new Array(type, "맥아더  ", "Lv.12", "   4만 석유",  1, 22));
+	taskList.push(new Array(type, "맥아더  ", "Lv.13", "   4만 석유",  1, 22));
+	taskList.push(new Array(type, "맥아더  ", "Lv.14", "   4만 석유",  1, 22));
+	taskList.push(new Array(type, "맥아더  ", "Lv.15", "   4만 석유",  1, 22));
+	taskList.push(new Array(type, "맥아더  ", "Lv.16", "   4만 석유",  1, 22));
+	taskList.push(new Array(type, "맥아더  ", "Lv.17", "   4만 석유",  1, 22));
+	taskList.push(new Array(type, "맥아더  ", "Lv.18", "   4만 석유",  1, 22));
+	taskList.push(new Array(type, "맥아더  ", "Lv.19", "   4만 석유",  1, 22));
+	taskList.push(new Array(type, "맥아더  ", "Lv.20", "   4만 석유",  1, 22));
+	taskList.push(new Array(type, "맥아더  ", "Lv.21", "   4만 석유",  1, 22));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
