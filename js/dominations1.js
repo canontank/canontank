f472b465076800044b4635bdb7dbe3320ddabe70
canontank <58597814+canontank@users.noreply.github.com>
2021-12-19 18:44:26 +0900

    Update dominations1.js

--- js/dominations1.js
+++ js/dominations1.js
@@ -88,18 +88,17 @@ function setTask21() {
 	var taskMap = new Object();
 	var type = "무기1";
 	taskMap.title = "2-1. 무기고 연구";
-	taskMap.startDate = "2021-12-17 22:37:00";
-	taskList.push(new Array(type, "맥아더  ", "Lv.20", "   4만 석유",  1, 22));
+	taskMap.startDate = "2021-12-19 18:43:00";
 	taskList.push(new Array(type, "맥아더  ", "Lv.21", "   4만 석유",  1, 22));
 	taskList.push(new Array(type, "노부나가", "Lv.19", "   4만 석유",  1, 22));
 	taskList.push(new Array(type, "노부나가", "Lv.20", "   4만 석유",  1, 22));
 	taskList.push(new Array(type, "노부나가", "Lv.21", "   4만 석유",  1, 22));
-	taskList.push(new Array(type, "주코프  ", "Lv. 2", "  23만 식량",  0,  8));
-	taskList.push(new Array(type, "주코프  ", "Lv. 3", "  50만 식량",  0,  8));
 	taskList.push(new Array(type, "유명개틀", "Lv.15", "  25만 석유", 17, 14));
 	taskList.push(new Array(type, "중전차  ", "Lv.13", "27.2만 석유", 17, 14));
 	taskList.push(new Array(type, "무장차량", "Lv.12", "27.2만 석유", 17, 14));
 	taskList.push(new Array(type, "엠알엘  ", "Lv. 8", "  26만 석유", 17, 14));
+	taskList.push(new Array(type, "주코프  ", "Lv. 2", "  23만 식량",  0,  8));
+	taskList.push(new Array(type, "주코프  ", "Lv. 3", "  50만 식량",  0,  8));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
