baebb9812e421d6300023ea129c5dcfcd87d54f9
canontank <58597814+canontank@users.noreply.github.com>
2022-03-30 02:11:32 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -20,7 +20,8 @@ function setTask12() {
 	taskMap.startDate = "2022-03-19 19:52:00";
 	taskList.push(new Array(type, "타워    ", "Lv.15", "1800만 골드", 13,  0));
 	taskList.push(new Array(type, "타워    ", "Lv.15", "1800만 골드", 13,  0));
-	taskList.push(new Array(type, "수비대  ", "Lv. 8", " 525만 골드",  8,  0));
+	taskList.push(new Array(type, "대학    ", "Lv. 9", "1000만 골드", 12, 12));
+	taskList.push(new Array(type, "대학    ", "Lv.10", "1000만 골드", 13,  0)); // 미확인
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
@@ -33,7 +34,8 @@ function setTask13() {
 	taskMap.startDate = "2022-03-26 21:56:00";
 	taskList.push(new Array(type, "타워    ", "Lv.15", "1800만 골드", 13,  0));
 	taskList.push(new Array(type, "타워    ", "Lv.15", "1800만 골드", 13,  0));
-	taskList.push(new Array(type, "수비대  ", "Lv. 8", " 525만 골드",  8,  0));
+	taskList.push(new Array(type, "의회    ", "Lv. 5", "1200만 골드", 14,  0));
+	taskList.push(new Array(type, "의회    ", "Lv. 6", "1300만 골드", 15,  0)); // 미확인
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
@@ -46,6 +48,7 @@ function setTask14() {
 	taskMap.startDate = "2022-03-23 23:10:00";
 	taskList.push(new Array(type, "타워    ", "Lv.15", "1800만 골드", 13,  0));
 	taskList.push(new Array(type, "타워    ", "Lv.15", "1800만 골드", 13,  0));
+	taskList.push(new Array(type, "수비대  ", "Lv. 8", " 525만 골드",  8,  0));
 	taskList.push(new Array(type, "수비대  ", "Lv. 8", " 525만 골드",  8,  0));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
@@ -60,6 +63,7 @@ function setTask15() {
 	taskList.push(new Array(type, "타워    ", "Lv.15", "1800만 골드", 13,  0));
 	taskList.push(new Array(type, "타워    ", "Lv.15", "1800만 골드", 13,  0));
 	taskList.push(new Array(type, "수비대  ", "Lv. 8", " 525만 골드",  8,  0));
+	taskList.push(new Array(type, "수비대  ", "Lv. 8", " 525만 골드",  8,  0));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
@@ -71,6 +75,8 @@ function setTask21() {
 	taskMap.title = "2-1. 무기고 연구";
 	taskMap.startDate = "2022-03-29 21:33:00";
 	taskList.push(new Array(type, "에티오피", "Lv. 4", "   6만 석유",  3, 20));
+	taskList.push(new Array(type, "미국    ", "Lv. 5", "   6만 석유",  7, 14));
+	taskList.push(new Array(type, "에티오피", "Lv. 5", "   6만 석유",  7, 14)); // 미확인
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
@@ -83,7 +89,9 @@ function setTask22() {
 	taskMap.startDate = "2022-03-17 08:10:00";
 	taskList.push(new Array(type, "고급헬기", "Lv. 2", "1800만 식량", 15,  6));
 	taskList.push(new Array(type, "필리핀  ", "Lv. 2", "   1만 석유",  0, 23));
-	taskList.push(new Array(type, "필리핀  ", "Lv. 3", "   1만 석유",  0, 23)); // 미확인
+	taskList.push(new Array(type, "필리핀  ", "Lv. 3", "   3만 석유",  2,  0)); // 미확인
+	taskList.push(new Array(type, "필리핀  ", "Lv. 4", "   6만 석유",  3, 20)); // 미확인
+	taskList.push(new Array(type, "필리핀  ", "Lv. 5", "   6만 석유",  7, 14)); // 미확인
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
