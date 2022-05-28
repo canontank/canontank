e675e61c825a07333caf2749bdc77d26f71e8f02
canontank <58597814+canontank@users.noreply.github.com>
2022-05-28 12:41:31 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -81,9 +81,9 @@ function setTask21() {
 	var type = "무기1";
 	taskMap.title = "2-1. 무기고 연구";
 	taskMap.startDate = "2022-05-28 12:20:00";
-	taskList.push(new Array(type, "체로키  ", "Lv. 7", " 800만 식량", 10, 10));
-	taskList.push(new Array(type, "미국    ", "Lv. 7", "  12만 석유", 10, 10)); // 미확인
-	taskList.push(new Array(type, "에티오피", "Lv. 7", "  12만 석유", 10, 10)); // 미확인
+	taskList.push(new Array(type, "체로키  ", "Lv. 7", " 800만 식량", 10, 11));
+	taskList.push(new Array(type, "미국    ", "Lv. 7", "  12만 석유", 10, 11)); // 미확인
+	taskList.push(new Array(type, "에티오피", "Lv. 7", "  12만 석유", 10, 11)); // 미확인
 	taskList.push(new Array(type, "아즈텍  ", "Lv. 8", "1000만 식량", 11, 12)); // 미확인
 	taskList.push(new Array(type, "몽골    ", "Lv. 8", "1000만 식량", 11, 12)); // 미확인
 	taskList.push(new Array(type, "체로키  ", "Lv. 8", "1000만 식량", 11, 12)); // 미확인
@@ -99,9 +99,9 @@ function setTask22() {
 	var type = "무기2";
 	taskMap.title = "2-2. 무기고 연구";
 	taskMap.startDate = "2022-05-28 12:20:00";
-	taskList.push(new Array(type, "마오리  ", "Lv. 7", " 800만 골드", 10, 10));
-	taskList.push(new Array(type, "인도    ", "Lv. 7", " 800만 골드", 10, 10));
-	taskList.push(new Array(type, "러시아  ", "Lv. 7", " 800만 골드", 10, 10)); // 미확인
+	taskList.push(new Array(type, "마오리  ", "Lv. 7", " 800만 골드", 10, 11));
+	taskList.push(new Array(type, "인도    ", "Lv. 7", " 800만 골드", 10, 11));
+	taskList.push(new Array(type, "러시아  ", "Lv. 7", " 800만 골드", 10, 11)); // 미확인
 	taskList.push(new Array(type, "러시아  ", "Lv. 8", "  11만 석유", 11, 12)); // 미확인
 	taskList.push(new Array(type, "필리핀  ", "Lv. 8", "  11만 석유", 11, 12)); // 미확인
 	taskList.push(new Array(type, "이집트  ", "Lv. 8", "1000만 골드", 11, 12)); // 미확인
