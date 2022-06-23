a541451f2ccc0fb4ce876c847720d592425ffe7a
canontank <58597814+canontank@users.noreply.github.com>
2022-06-24 01:41:53 +0900

    Update dominations1.js

--- js/dominations1.js
+++ js/dominations1.js
@@ -17,8 +17,7 @@ function setTask12() {
 	var taskMap = new Object();
 	var type = "국민2";
 	taskMap.title = "1-2. 국민 (6명)";
-	taskMap.startDate = "2022-06-09 07:08:00";
-	taskList.push(new Array(type, "수비대  ", "Lv.13", "1600만 골드", 15, 12));
+	taskMap.startDate = "2022-06-23 23:08:00";
 	taskList.push(new Array(type, "수비대  ", "Lv.13", "1600만 골드", 15, 12));
 	taskList.push(new Array(type, "수비대  ", "Lv.14", "1800만 골드", 16,  0));
 	taskList.push(new Array(type, "수비대  ", "Lv.14", "1800만 골드", 16,  0));
@@ -46,8 +45,7 @@ function setTask14() {
 	var taskMap = new Object();
 	var type = "국민4";
 	taskMap.title = "1-4. 국민 (1명)";
-	taskMap.startDate = "2022-06-13 19:03:00";
-	taskList.push(new Array(type, "가시함정", "Lv. 9", " 250만 골드",  9,  0));
+	taskMap.startDate = "2022-06-24 01:38:00";
 	taskList.push(new Array(type, "가시함정", "Lv.10", " 300만 골드", 10,  0));
 	taskList.push(new Array(type, "가시함정", "Lv.11", " 370만 골드", 10, 12));
 	taskList.push(new Array(type, "가시함정", "Lv.12", " 425만 골드", 11,  0));
@@ -65,8 +63,7 @@ function setTask21() {
 	var taskMap = new Object();
 	var type = "무기1";
 	taskMap.title = "2-1. 무기고 연구";
-	taskMap.startDate = "2022-06-20 22:42:00";
-	taskList.push(new Array(type, "노부나가", "Lv.24", " 5.5만 석유",  2, 20));
+	taskMap.startDate = "2022-06-24 01:38:00";
 	taskList.push(new Array(type, "노부나가", "Lv.25", " 5.5만 석유",  2, 20));
 	taskList.push(new Array(type, "노부나가", "Lv.26", " 5.5만 석유",  2, 20));
 	taskList.push(new Array(type, "노부나가", "Lv.27", " 5.5만 석유",  2, 20));
@@ -82,8 +79,7 @@ function setTask22() {
 	var taskMap = new Object();
 	var type = "무기2";
 	taskMap.title = "2-2. 무기고 연구";
-	taskMap.startDate = "2022-06-20 22:42:00";
-	taskList.push(new Array(type, "나폴레옹", "Lv.24", " 5.5만 석유",  2, 20));
+	taskMap.startDate = "2022-06-24 01:38:00";
 	taskList.push(new Array(type, "나폴레옹", "Lv.25", " 5.5만 석유",  2, 20));
 	taskList.push(new Array(type, "나폴레옹", "Lv.26", " 5.5만 석유",  2, 20));
 	taskList.push(new Array(type, "나폴레옹", "Lv.27", " 5.5만 석유",  2, 20));
