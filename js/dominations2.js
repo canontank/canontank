8dcfa20184194b6052bc62074f7ef53772c14bee
canontank <58597814+canontank@users.noreply.github.com>
2022-07-24 22:28:16 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -80,12 +80,23 @@ function setTask21() {
 	var taskMap = new Object();
 	var type = "무기1";
 	taskMap.title = "2-1. 무기고 연구";
-	taskMap.startDate = "2022-07-17 07:26:00";
-	taskList.push(new Array(type, "선봉수송", "Lv. 2", "  20만 석유",  9, 12));
+	taskMap.startDate = "2022-07-24 22:27:00";
 	taskList.push(new Array(type, "선봉수송", "Lv. 3", "  20만 석유",  9, 12));
 	taskList.push(new Array(type, "선봉수송", "Lv. 4", "  20만 석유",  9, 12));
 	taskList.push(new Array(type, "선봉수송", "Lv. 5", "  20만 석유",  9, 12));
 	taskList.push(new Array(type, "선봉수송", "Lv. 6", "  20만 석유",  9, 12));
+	taskList.push(new Array(type, "선봉폭격", "Lv. 1", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉폭격", "Lv. 2", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉폭격", "Lv. 3", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉폭격", "Lv. 4", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉폭격", "Lv. 5", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉폭격", "Lv. 6", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉정찰", "Lv. 1", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉정찰", "Lv. 2", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉정찰", "Lv. 3", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉정찰", "Lv. 4", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉정찰", "Lv. 5", "  20만 석유",  9, 11));
+	taskList.push(new Array(type, "선봉정찰", "Lv. 6", "  20만 석유",  9, 11));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
@@ -101,6 +112,14 @@ function setTask22() {
 	taskList.push(new Array(type, "선봉전투", "Lv. 4", "  20만 석유",  9, 12));
 	taskList.push(new Array(type, "선봉전투", "Lv. 5", "  20만 석유",  9, 12));
 	taskList.push(new Array(type, "선봉전투", "Lv. 6", "  20만 석유",  9, 12));
+	taskList.push(new Array(type, "바주카  ", "Lv. 1", "  28만 석유", 18,  2));
+	taskList.push(new Array(type, "게릴라  ", "Lv. 1", "  28만 석유", 18,  2));
+	taskList.push(new Array(type, "개틀링  ", "Lv. 1", "  28만 석유", 18,  2));
+	taskList.push(new Array(type, "중전차  ", "Lv. 1", "  28만 석유", 18,  2));
+	taskList.push(new Array(type, "무장차량", "Lv. 1", "  28만 석유", 18,  2));
+	taskList.push(new Array(type, "에이피씨", "Lv. 1", "  28만 석유", 18,  2));
+	taskList.push(new Array(type, "엠알엘  ", "Lv. 1", "  28만 석유", 18,  2));
+	taskList.push(new Array(type, "전술헬기", "Lv. 1", "  28만 석유", 18,  2));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
