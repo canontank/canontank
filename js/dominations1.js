5c61718628327b8b689fcaa266393695bd00199a
canontank <58597814+canontank@users.noreply.github.com>
2023-02-02 21:34:05 +0900

    Update dominations1.js

--- js/dominations1.js
+++ js/dominations1.js
@@ -92,10 +92,9 @@ function setTask21() {
 	var taskMap = new Object();
 	var type = "무기1";
 	taskMap.title = "2-1. 무기고 연구";
-	taskMap.startDate = "2023-01-17 23:15:00";
-	taskList.push(new Array(type, "드론궁수", "Lv. 1", "1520만 식량", 16,  4));
-	taskList.push(new Array(type, "드론돌격", "Lv. 1", "1600만 식량", 16,  4));	
-	taskList.push(new Array(type, "드론공병", "Lv. 1", "1600만 식량", 16,  4));
+	taskMap.startDate = "2023-02-02 21:30:00";
+	taskList.push(new Array(type, "드론강습", "Lv. 1", "2230만 식량", 16,  4));
+	taskList.push(new Array(type, "드론돌격", "Lv. 1", "1600만 식량", 16,  4));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
@@ -108,7 +107,7 @@ function setTask22() {
 	taskMap.startDate = "2023-01-29 19:16:00";
 	taskList.push(new Array(type, "선봉정찰", "Lv. 6", "  20만 석유",  9, 11));
 	taskList.push(new Array(type, "드론헬기", "Lv. 1", "2230만 식량", 16,  4));
-	taskList.push(new Array(type, "드론강습", "Lv. 1", "2230만 식량", 16,  4));
+	taskList.push(new Array(type, "드론공병", "Lv. 1", "1600만 식량", 16,  4));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
