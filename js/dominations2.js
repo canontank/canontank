6a63d5527a1c8e852b90b5484a363c5a26a3179f
canontank <58597814+canontank@users.noreply.github.com>
2023-03-02 00:17:34 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -49,9 +49,8 @@ function setTask14() {
 	var taskMap = new Object();
 	var type = "국민4";
 	taskMap.title = "1-4. 국민 (3명)";
-	taskMap.startDate = "2023-02-27 22:11:00";
-	taskList.push(new Array(type, "대전차포", "Lv. 3", "  75만 골드",  2,  2));
-	taskList.push(new Array(type, "대전차포", "Lv. 4", " 290만 골드",  4,  0));
+	taskMap.startDate = "2023-03-02 00:11:00";
+	taskList.push(new Array(type, "대전차포", "Lv. 4", " 290만 골드",  3,  5));
 	taskList.push(new Array(type, "대전차포", "Lv. 5", " 450만 골드", 10,  0));
 	taskList.push(new Array(type, "대전차포", "Lv. 6", " 600만 골드", 12,  0));
 	taskList.push(new Array(type, "대전차포", "Lv. 7", " 725만 골드", 13,  0));
@@ -69,9 +68,9 @@ function setTask15() {
 	var taskMap = new Object();
 	var type = "국민5";
 	taskMap.title = "1-5. 국민 (1명)";
-	taskMap.startDate = "2023-02-16 23:30:00";
-	taskList.push(new Array(type, "매복함정", "Lv.11", " 650만 골드", 13,  0));
+	taskMap.startDate = "2023-03-02 00:11:00";
 	taskList.push(new Array(type, "매복함정", "Lv.12", " 700만 골드", 13, 12));
+	taskList.push(new Array(type, "매복함정", "Lv.13", " 800만 골드", 14,  0));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
 }
