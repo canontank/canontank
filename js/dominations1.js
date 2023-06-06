f1c503ade12694149892f068f12efa44b34b18a5
canontank <58597814+canontank@users.noreply.github.com>
2023-06-07 00:05:04 +0900

    Update dominations1.js

--- js/dominations1.js
+++ js/dominations1.js
@@ -47,7 +47,7 @@ function setTask14() {
 	var taskMap = new Object();
 	var type = "국민4";
 	taskMap.title = "1-4. 국민 (3명)";
-	taskMap.startDate = "2023-06-07 23:50:00";
+	taskMap.startDate = "2023-06-06 23:50:00";
 	taskList.push(new Array(type, "대전차포", "Lv.10", "1600만 골드", 16, 12));
 	taskList.push(new Array(type, "대전차포", "Lv.11", "1900만 골드", 17,  0));
 	taskList.push(new Array(type, "대전차포", "Lv.12", "2200만 골드", 18,  0));
@@ -74,7 +74,7 @@ function setTask21() {
 	var taskMap = new Object();
 	var type = "무기1";
 	taskMap.title = "2-1. 무기고 연구";
-	taskMap.startDate = "2023-06-07 23:50:00";
+	taskMap.startDate = "2023-06-06 23:50:00";
 	taskList.push(new Array(type, "맥아더  ", "Lv.30", "   7만 석유",  2, 20));
 	taskList.push(new Array(type, "처칠    ", "Lv.22", " 5.5만 석유",  2, 20));
 	taskList.push(new Array(type, "처칠    ", "Lv.23", " 5.5만 석유",  2, 20));
@@ -87,7 +87,7 @@ function setTask22() {
 	var taskMap = new Object();
 	var type = "무기2";
 	taskMap.title = "2-2. 무기고 연구";
-	taskMap.startDate = "2023-06-07 23:50:00";
+	taskMap.startDate = "2023-06-06 23:50:00";
 	taskList.push(new Array(type, "잔다르크", "Lv.30", "   7만 석유",  2, 20));
 	taskList.push(new Array(type, "주코프  ", "Lv.22", " 5.5만 석유",  2, 20));
 	taskList.push(new Array(type, "주코프  ", "Lv.23", " 5.5만 석유",  2, 20));
