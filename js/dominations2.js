d1d8e1788e19cf603a44fa41922049a559e8c720
canontank <58597814+canontank@users.noreply.github.com>
2024-09-10 19:28:09 +0900

    Update dominations2.js

--- js/dominations2.js
+++ js/dominations2.js
@@ -72,15 +72,14 @@ function setTask21() {
 	var taskMap = new Object();
 	var type = "무기1";
 	taskMap.title = "2-1. 무기고 연구";
-	taskMap.startDate = "2024-08-22 21:18:00";
-	taskList.push(new Array(type, "에이피씨", "Lv. 1", "  30만 석유", 19,  0));
+	taskMap.startDate = "2024-09-10 19:25:00";
+	taskList.push(new Array(type, "엠알엘  ", "Lv.19", "  30만 석유", 19, 22));
 	taskList.push(new Array(type, "개틀링  ", "Lv.19", "  30만 석유", 19, 22));
 	taskList.push(new Array(type, "게릴라  ", "Lv.19", "  30만 석유", 19, 22));
 	taskList.push(new Array(type, "중전차  ", "Lv.19", "  30만 석유", 19, 22));
 	taskList.push(new Array(type, "바주카  ", "Lv.19", "  30만 석유", 19, 22));
 	taskList.push(new Array(type, "무장차량", "Lv.19", "  30만 석유", 19, 22));
 	taskList.push(new Array(type, "에이피씨", "Lv.19", "  30만 석유", 19, 22));
-	taskList.push(new Array(type, "엠알엘  ", "Lv.19", "  30만 석유", 19, 22));
 	taskList.push(new Array(type, "전술헬기", "Lv.19", "  30만 석유", 19, 22));
 	taskMap.taskList = taskList;
 	allTaskMapList.push(taskMap);
